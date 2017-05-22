b52072f8f84dadf703133744760e74467c6bfac3
Slava Kovalenko <slava.kovalenko@architech.nyc>
2017-05-22 19:04:50 +0300

    style generation proof of concept

---
+++ src/styles/index.ts
@@ -0,0 +1,145 @@
+// import {StyleSheet} from 'react-native';
+// import {ViewStyle, TextStyle, ImageStyle} from '@types/react-native/index';
+
+export interface Multiplicator {
+  [key: string]: number,
+}
+
+export interface Color {
+  [key: string]: string,
+}
+
+export interface Style {
+  [key: string]: number | string,
+}
+
+export interface StylesResult {
+  [key: string]: Style,
+}
+
+const defaultMultiplicators: Multiplicator[] = [
+  {
+    '0': 0,
+  },
+  {
+    '05': 0.5,
+  },
+  {
+    '075': 0.75,
+  },
+  {
+    '1': 1,
+  },
+];
+
+const result = (
+  remSize: number = 16
+  fontNames: string[] = [],
+  colors: Color[] = [],
+): StylesResult => {
+
+  const borderStyles = generateBorderStyles(defaultMultiplicators);
+  const borderRadiusStyles = generateBorderRadiusStyles(multiplyToRem(remSize, defaultMultiplicators));
+  const fontSizeStyles = generateFontSizeStyles(multiplyToRem(remSize, defaultMultiplicators));
+
+  return {
+    ...borderStyles,
+    ...borderRadiusStyles,
+    ...fontSizeStyles,
+  };
+};
+
+const generateBorderStyles = (multiplicators: Multiplicator[]): StylesResult => {
+  return multiplyStylesValues({
+    ba: {
+        borderWidth: 1,
+    },
+    bt: {
+        borderTopWidth: 1,
+    },
+    br: {
+        borderRightWidth: 1,
+    },
+    bb: {
+        borderBottomWidth: 1,
+    },
+    bl: {
+        borderLeftWidth: 1,
+    },
+  }, multiplicators);
+};
+
+const generateBorderRadiusStyles = (multiplicators: Multiplicator[]): StylesResult => {
+  return multiplyStylesValues({
+    br: {
+        borderRadius: 1,
+    },
+    'br--bottom': {
+        borderTopLeftRadius: 0,
+        borderTopRightRadius: 0,
+    },
+    'br--top': {
+        borderBottomLeftRadius: 0,
+        borderBottomRightRadius: 0,
+    },
+    'br--left': {
+        borderTopRightRadius: 0,
+        borderBottomRightRadius: 0,
+    },
+    'br--right': {
+        borderTopLeftRadius: 0,
+        borderBottomLeftRadius: 0,
+    },
+  },
+  multiplicators);
+};
+
+const generateFontSizeStyles = (multiplicators: Multiplicator[]): StylesResult => {
+  return multiplyStylesValues({
+    fs: {
+      fontSize: 1,
+    },
+  },
+  multiplicators);
+};
+
+const multiplyStylesValues = (styles: StylesResult, multiplicators: Multiplicator[]): StylesResult => {
+  const resultStyles: StylesResult = {};
+  Object.keys(styles).map(key => {
+    if (key.includes('--')) {
+      resultStyles[key] = styles[key];
+    }
+    else {
+      multiplicators.map(multiplicator => {
+        const prefix: string = Object.keys(multiplicator)[0];
+        const multiplicatorValue: number = multiplicator[prefix];
+        const multiplyedStyle: Style = {};
+
+        Object.keys(styles[key]).map((styleKey: string) => {
+          const oldStyleValue = styles[key][styleKey];
+          if (typeof oldStyleValue === 'number') {
+            multiplyedStyle[styleKey] = oldStyleValue * multiplicatorValue;
+          }
+          else {
+            multiplyedStyle[styleKey] = oldStyleValue;
+          }
+        });
+
+        resultStyles[key + prefix] = multiplyedStyle;
+      });
+    }
+  });
+  return resultStyles;
+};
+
+const multiplyToRem = (remValue: number, multiplicators: Multiplicator[]): Multiplicator[] => {
+  return multiplicators.map(multiplicator => {
+    const prefix: string = Object.keys(multiplicator)[0];
+    const multiplicatorValue: number = multiplicator[prefix];
+    return {
+      prefix: multiplicatorValue * remValue,
+    };
+  });
+};
+
+export default result;
